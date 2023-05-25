647fd7cce5f2ee16c8dfc287d88b014d89b5b222
tim714 <ildarnigmetzjanov@gmail.com>
2023-05-25 13:08:20 +0300

    Правка

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-# from django.views.generic import TemplateView
 from drf_yasg.views import get_schema_view
 from django.conf.urls import url
 from drf_yasg import openapi
@@ -8,10 +7,9 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Cats API",
+      title="API",
       default_version='v1',
       description="Документация для приложения cats проекта Kittygram",
-      # terms_of_service="URL страницы с пользовательским соглашением",
       contact=openapi.Contact(email="admin@kittygram.ru"),
       license=openapi.License(name="BSD License"),
    ),
@@ -22,19 +20,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
-    # path(
-    #     'redoc/',
-    #     TemplateView.as_view(template_name='redoc.html'),
-    #     name='redoc'
-    # )
 ]
 
 
 urlpatterns += [
    url(r'^swagger(?P<format>\.json|\.yaml)$', 
        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
        name='schema-swagger-ui'),
-   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
        name='schema-redoc'),
 ] 
